e1259fe865cfb7295de27d6bf69da5595ba7efde
raozhaizhu <raozhaizhu@gmail.com>
2026-08-27 14:26:50 +0800

    重写告一段落,dto 和 service 部分做了必要重写,加入了 tree_path,并修复一下之前写简单 crud 时没写的 parent_node 逻辑

---
+++ src/main/resources/db/migration/V1.0.6__init_supplier_data.sql
@@ -0,0 +1,8 @@
+-- V1.0.6__init_supplier_data.sql
+-- Author: raozhaizhu
+-- Date: 2026/8/27 13:19
+
+CREATE TABLE `` (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '主键',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='';
